88fe6670d3beed8f91ef7bb41e5b87951025bada
dkfkfk12 <107744891+dkfkfk12@users.noreply.github.com>
2022-06-18 18:18:32 +0900

    Delete event.js

--- event.js
+++
@@ -1,7 +0,0 @@
-function openPopup() {
-    var _width = '830';
-    var _height = '730';
-    var _left = Math.ceil((window.screen.width - _width)/2);
-    var _top = Math.ceil((window.screen.height - _height)/2);
-    window.open('notice.html', 'popup-test', 'width='+ _width+', height='+ _height+',left='+ _left+', top='+_top);
-}
